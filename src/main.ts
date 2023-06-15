5adf094cdd869ab755cbdbfdfdda63cc155a73d6
MehradFarahnak <mehradfarahnak9@gmail.com>
2023-06-15 15:58:30 +0330

    test workflow

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api/v1/');
+  console.log('SALAM');
   await app.listen(3000);
 }
 bootstrap();
