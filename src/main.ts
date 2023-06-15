ffb5caa49dfc82af8b9f92ce0c15243cb4e481c7
MehradFarahnak <mehradfarahnak9@gmail.com>
2023-06-15 16:01:42 +0330

    change port

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api/v1/');
   console.log('SALAM');
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
